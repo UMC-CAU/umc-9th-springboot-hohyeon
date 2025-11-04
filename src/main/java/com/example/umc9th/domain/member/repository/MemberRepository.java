415b97f83ec78055ca2832b228f92fe5aaab78df
Seojun Choi <tjwns7759@cau.ac.kr>
2025-11-04 21:36:48 +0900

    MemberRepository 메소드 시그니처에 Optional 추가

--- src/main/java/com/example/umc9th/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/umc9th/domain/member/repository/MemberRepository.java
@@ -3,8 +3,16 @@ package com.example.umc9th.domain.member.repository;
 import com.example.umc9th.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findMemberById(Long id);
+    Optional<Member> findMemberById(Long id);
+
+    /*
+    Optional을 추가한 이유?
+    : 데이터베이스에 해당 멤버의 ID가 없으면 이 메서드가 null을 반환하여 NPE 발생
+     */
+
     /*
     JPA는 ORM(Object-Relational Mapping), 즉 데이터베이스의 레코드(Row)와 Java의 객체(Object)를 1:1로 매핑하는 기술.
     findById의 임무는 "ID가 ...인 레코드를 찾아서, 완벽한 Member 객체 1개를 만들어라"
