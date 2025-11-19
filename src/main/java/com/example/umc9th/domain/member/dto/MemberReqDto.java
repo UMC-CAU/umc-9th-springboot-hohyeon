d7c67a1fc11d991ca1465267aecab94d27197d9c
Seojun Choi <tjwns7759@cau.ac.kr>
2025-11-19 17:22:29 +0900

    feat: 회원가입을 위한 DTO 생성(MemberReqDto, MemberResDto)

--- src/main/java/com/example/umc9th/domain/member/dto/MemberReqDto.java
+++ src/main/java/com/example/umc9th/domain/member/dto/MemberReqDto.java
@@ -1,4 +1,17 @@
 package com.example.umc9th.domain.member.dto;
 
+import com.example.umc9th.domain.member.enums.Gender;
+
+import java.time.LocalDate;
+
 public class MemberReqDto {
+    public record JoinDto(
+            String name,
+            String nickname,
+            Gender gender,
+            LocalDate birth,
+            String detailAddress,
+            String email,
+            String phoneNumber
+    ){}
 }
