91b107993137e808bf83552975cc83d0b343ce83
Seojun Choi <tjwns7759@cau.ac.kr>
2025-11-28 03:28:45 +0900

    feat: 특정 가게의 미션 목록 API swagger 명세 추가

---
+++ src/main/java/com/example/umc9th/domain/mission/controller/MissionControllerDocs.java
@@ -0,0 +1,19 @@
+package com.example.umc9th.domain.mission.controller;
+
+import com.example.umc9th.domain.review.dto.ReviewResDto;
+import com.example.umc9th.global.apiPayLoad.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+public interface MissionControllerDocs {
+    // 가게의 리뷰 목록 조회
+    @Operation(
+            summary = "특정 가게의 미션 목록 조회 API",
+            description = "특정 가게의 미션을 모두 조회합니다. 페이지네이션으로 제공합니다."
+    )
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "실패")
+    })
+    ApiResponse<ReviewResDto.ReviewPreViewListDto> getStoreMissions(Long storeId, Integer page);
+}
