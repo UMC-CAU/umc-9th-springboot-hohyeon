be2b6cec2cd724d8dde9eb2b6267a0d4c5a081bd
Seojun Choi <tjwns7759@cau.ac.kr>
2025-11-28 01:36:27 +0900

    feat: 내가 작성한 리뷰 목록 API swagger 명세 추가

--- src/main/java/com/example/umc9th/domain/review/controller/ReviewControllerDocs.java
+++ src/main/java/com/example/umc9th/domain/review/controller/ReviewControllerDocs.java
@@ -8,7 +8,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 public interface ReviewControllerDocs {
     // 가게의 리뷰 목록 조회
     @Operation(
-            summary = "가게의 리뷰 목록 조회 API By 마크 (개발 중)",
+            summary = "가게의 리뷰 목록 조회 API",
             description = "특정 가게의 리뷰를 모두 조회합니다. 페이지네이션으로 제공합니다."
     )
     @ApiResponses({
@@ -16,5 +16,14 @@ public interface ReviewControllerDocs {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "실패")
     })
     ApiResponse<ReviewResDto.ReviewPreViewListDto> getStoreReviews(String storeName, Integer page);
+
+    @Operation(
+            summary = "내가 작성한 리뷰 목록 조회 API",
+            description = "내가 작성한 리뷰를 모두 조회합니다. 페이지네이션으로 제공합니다."
+    )
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "실패")
+    })
     ApiResponse<ReviewResDto.ReviewPreViewListDto> getMyReviews(Long memberId,  Integer page);
 }
